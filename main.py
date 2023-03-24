a249a414ad50aeea96ba5d4de80534b2051ec0bc
sxmurxi <aleksandr.tentser@gmail.com>
2023-03-24 14:39:46 +0200

    Wrote basic script using pyrogram

---
+++ main.py
@@ -0,0 +1,17 @@
+from configparser import ConfigParser
+from pyrogram import Client, filters
+
+config = ConfigParser()
+
+config.read('config.ini')
+
+api_id = config.get('pyrogram', 'api_id')
+api_hash = config.get('pyrogram', 'api_hash')
+
+app = Client(name='my_account', api_id=api_id, api_hash=api_hash)
+text = 'Норм)'
+@app.on_message(filters=filters.web_page)
+async def auto_answer(event, message):
+        await app.send_message(chat_id=message.chat.id, text=text)
+
+app.run()
